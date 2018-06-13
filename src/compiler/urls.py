96dc7bb756719433871b877f9095f52f24cba5c1
“vivek0079” <vivek.vivek0079@gmail.com>
2018-06-13 18:33:28 +0530

    Minor change

--- src/compiler/urls.py
+++ src/compiler/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 
-from .views import index, executeCode, runCode, viewSavedCode
+from .views import index, executeCode, viewSavedCode
 
 app_name = 'compiler'
 
